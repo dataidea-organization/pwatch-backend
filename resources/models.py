b1c3dbccbe068648f113f78f5c8000a20719557f
jumashafara <jumashafara0@gmail.com>
2025-12-11 16:29:09 +0300

    feat: initialized resources models

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+class Explainers(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    file = models.FileField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Explainer'
+        ordering = ['-created_at']
+
+
+class Report(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    file = models.FileField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Report'
+        ordering = ['-created_at']
+
+
+class PartnerPublication(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    file = models.FileField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'PartnerPublication'
+        ordering = ['-created_at']
+
+
+class Statement(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    file = models.FileField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Statement'
+        ordering = ['-created_at']
+
